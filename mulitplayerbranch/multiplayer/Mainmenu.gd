f0070cd020039af276d6dec3f32701d2f6f97445
Gawasinspire <gavaskar2018@gmail.com>
2020-06-20 13:18:27 +0200

    separate screen update

--- mulitplayerbranch/multiplayer/Mainmenu.gd
+++ mulitplayerbranch/multiplayer/Mainmenu.gd
@@ -6,5 +6,14 @@ func _on_ExplorerButton_pressed():
 func _on_FamilyButton_pressed():
 	_load_game()
 
+
+func _on_FriendsButton_pressed():
+	_load_game()
+
+func _on_ExitButton_pressed():
+	queue_free()
+
+
 func _load_game():
 	get_tree().change_scene('res://interface/Menu.tscn')
+
--- mulitplayerbranch/multiplayer/QuizRoom.gd
+++ mulitplayerbranch/multiplayer/QuizRoom.gd
@@ -1,30 +1,60 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var template = [
+		{"options" :  ["Eric","Informatik","Godot"],
+		"question" : "0Once upon a time %s, a %s Student entered a Village called Clausthal. The Charming boy has no friends in mountain village clausthal but eagerly searching for one new friend. He is interested in learning %s"
+		},
+		{"question": "1There once was %s called %s who searched far and wide for wide for the mythical %s noun of %s",
+		"options" : ["a noun","a name","an adjective", "another name"]
+		},
+		{"question": "2There once was %s called %s who searched far and wide for wide for the mythical %s noun of %s",
+		"options" : ["a noun","a name","an adjective", "another name"]
+		}
+		]
+var current_question  
 
-# Called when the node enters the scene tree for the first time.
+var player_words = []
+
+onready var Question_panel = $VBoxContainer/QuestionPanel 
+var a = 1
+
+#var num = 42
+
+
 func _ready():
 	if is_network_master():
-		$VBoxContainer/QuestionPanel.text = "a"
-		$VBoxContainer/HBoxContainer/Answer.text = "b"
+		pick_question()
+		Question_panel.text = "What is your favourite fruit?"
+		$VBoxContainer/HBoxContainer/Answer1.text = "apple"
+		$VBoxContainer/HBoxContainer2/Answer2.text = "Mango"
+		$VBoxContainer/HBoxContainer3/Answer3.text = "Banana"
+		$VBoxContainer/HBoxContainer4/Anwer4.text = "None"
 	else:
-		$VBoxContainer/QuestionPanel.text = "b"
-		$VBoxContainer/HBoxContainer/Answer.text = "a"
-	
-	
-	
+		Question_panel.text = "What is your"+ "partners" +"favourite fruit?"
+		$VBoxContainer/HBoxContainer/Answer1.text = "apple"
+		$VBoxContainer/HBoxContainer2/Answer2.text = "Mango"
+		$VBoxContainer/HBoxContainer3/Answer3.text = "Banana"
+		$VBoxContainer/HBoxContainer4/Anwer4.text = "None"
+		
+
+func pick_question():
+	randomize()
+	current_question = template[randi() % template.size()]
+
+func update_score():
+	$GUI/Control/bannerHanging/HBoxContainer/score.text = "1"
+		
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-
 func _on_AnswerButton1_pressed():
-	pass # Replace with function body.
+	if is_network_master():
+		$VBoxContainer/HBoxContainer/Answer1.text = "jjj"
 
+	
 
 func _on_AnswerButton2_pressed():
 	pass # Replace with function body.
